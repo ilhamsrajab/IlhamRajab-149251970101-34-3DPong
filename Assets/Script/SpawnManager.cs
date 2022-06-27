8a46307cae98ce553d23684c74977d8e00104a66
Ilham Rajab <iamidat@outlook.com>
2022-06-27 18:36:40 +0700

    update

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -11,33 +11,5 @@ public class SpawnManager : MonoBehaviour
   public Vector3 areaMax;
   private float timer;
 
-  // Start is called before the first frame update
-  private void Start()
-  {
-    timer = 0;
-  }
 
-  // Update is called once per frame
-  private void Update()
-  {
-    timer += Time.deltaTime;
-
-    if (timer > spawnInterval)
-    {
-      timer -= spawnInterval;
-    }
-  }
-
-  public void GenerateSpawnBall()
-  {
-    GenerateSpawnBall(new Vector3(Random.Range(areaMin.x, areaMax.x), Random.Range(areaMin.y, areaMax.y)));
-  }
-
-  public void GenerateSpawnBall(Vector3 position)
-  {
-    if (true)
-    {
-
-    }
-  }
 }
